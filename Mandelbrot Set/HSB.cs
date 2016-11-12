1f085fee2602c7e8d157972f1b9739dcdad1cf44
Thomas Samsohang <samsohangthomas@gmail.com>
2016-11-13 00:22:43 +0545

    Decomposition is performed HSB.cs class and partial class MouseEvents.cs is created

---
+++ Mandelbrot Set/HSB.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrot_Set
+{
+
+    class HSB
+    {
+        public float rChan, gChan, bChan;
+        public HSB()
+        {
+            rChan = gChan = bChan = 0;
+        }
+        public void HSBTORGB(float h, float s, float b)
+        {
+            if (s == 0)
+            {
+                rChan = gChan = bChan = (int)(b * 255.0f + 0.5f);
+            }
+            else
+            {
+                h = (h - (float)Math.Floor(h)) * 6.0f;
+                float f = h - (float)Math.Floor(h);
+                float p = b * (1.0f - s);
+                float q = b * (1.0f - s * f);
+                float t = b * (1.0f - (s * (1.0f - f)));
+                switch ((int)h)
+                {
+                    case 0:
+                        rChan = (int)(b * 255.0f + 0.5f);
+                        gChan = (int)(t * 255.0f + 0.5f);
+                        bChan = (int)(p * 255.0f + 0.5f);
+                        break;
+                    case 1:
+                        rChan = (int)(q * 255.0f + 0.5f);
+                        gChan = (int)(b * 255.0f + 0.5f);
+                        bChan = (int)(p * 255.0f + 0.5f);
+                        break;
+                    case 2:
+                        rChan = (int)(p * 255.0f + 0.5f);
+                        gChan = (int)(b * 255.0f + 0.5f);
+                        bChan = (int)(t * 255.0f + 0.5f);
+                        break;
+                    case 3:
+                        rChan = (int)(p * 255.0f + 0.5f);
+                        gChan = (int)(q * 255.0f + 0.5f);
+                        bChan = (int)(b * 255.0f + 0.5f);
+                        break;
+                    case 4:
+                        rChan = (int)(t * 255.0f + 0.5f);
+                        gChan = (int)(p * 255.0f + 0.5f);
+                        bChan = (int)(b * 255.0f + 0.5f);
+                        break;
+                    case 5:
+                        rChan = (int)(b * 255.0f + 0.5f);
+                        gChan = (int)(p * 255.0f + 0.5f);
+                        bChan = (int)(q * 255.0f + 0.5f);
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ Mandelbrot Set/MouseEvents.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mandelbrot_Set
+{
+    partial class MandelbrotSet
+    {
+
+        // mouse Events 
+        private void pictureOutputBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            //e.consume();
+            if (action)
+            {
+                mousePressed = true;
+                xs = e.X;
+                ys = e.Y;
+            }
+        }
+
+        private void pictureOutputBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            //e.consume();
+            if (action && mousePressed)
+            {
+                xe = e.X;
+                ye = e.Y;
+                rectangle = true;
+                pictureOutputBox.Refresh();
+            }
+        }
+
+        private void pictureOutputBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            int z, w;
+
+            //e.consume();
+            if (action && mousePressed)
+            {
+                xe = e.X;
+                ye = e.Y;
+                if (xs > xe)
+                {
+                    z = xs;
+                    xs = xe;
+                    xe = z;
+                }
+                if (ys > ye)
+                {
+                    z = ys;
+                    ys = ye;
+                    ye = z;
+                }
+                w = (xe - xs);
+                z = (ye - ys);
+                if ((w < 2) && (z < 2)) initvalues();
+                else
+                {
+                    if (((float)w > (float)z * xy)) ye = (int)((float)ys + (float)w / xy);
+                    else xe = (int)((float)xs + (float)z * xy);
+                    xende = xstart + xzoom * (double)xe;
+                    yende = ystart + yzoom * (double)ye;
+                    xstart += xzoom * (double)xs;
+                    ystart += yzoom * (double)ys;
+                }
+                xzoom = (xende - xstart) / (double)x1;
+                yzoom = (yende - ystart) / (double)y1;
+                mandelbrot();
+                rectangle = false;
+                pictureOutputBox.Refresh();
+                mousePressed = false;
+            }
+        }
+
+    }
+}
